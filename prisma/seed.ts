4e8f24143e53ec2a9fd77b101ebe9a236da4f4a4
nottoezz <liam@birch.co.za>
2026-01-25 12:54:05 +0200

    seeding: ensure import correct prisma client

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,7 +10,7 @@
 
 // Imports
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { hash } from "bcryptjs";
 
