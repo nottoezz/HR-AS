185c9025da44b112aa85bff63cea6d7330231361
nottoezz <liam@birch.co.za>
2026-01-25 12:59:41 +0200

    prisma: add sqlite adapter to prismaclient constructor

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,8 +1,12 @@
 import { env } from "@/env";
 import { PrismaClient } from "../../generated/prisma";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
 const createPrismaClient = () =>
   new PrismaClient({
+    adapter: new PrismaBetterSqlite3({
+      url: env.DATABASE_URL,
+    }),
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
