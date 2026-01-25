7b8a3a563d3aa117ba933f0e5fc2d6126e1e67ac
nottoezz <liam@birch.co.za>
2026-01-25 12:28:14 +0200

    seeding: setup main seed function to create hr admin

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -33,8 +33,51 @@ const createPrismaClient = () => {
 
 // Main seed function
 // ------------------------------------------------------
+const main = async () => {
+  const prisma = createPrismaClient();
 
+  // Seed HR Admin
+  try {
+    console.log("Starting seed...");
+    const hashedPassword = await hash("TestPass1234", 12);
+    const hrAdmin = await prisma.user.upsert({
+      where: {
+        email: "hradmin@test.com",
+      },
+      // Update existing HR Admin if they exist
+      update: {
+        passwordHash: hashedPassword,
+        role: "HRADMIN",
+        employeeId: null,
+      },
+      // Create new HR Admin if they don't exist
+      create: {
+        email: "hradmin@test.com",
+        passwordHash: hashedPassword,
+        role: "HRADMIN",
+        employeeId: null,
+      },
+    });
+
+    // Log success
+    console.log("HR Admin successfully seeded!", {
+      id: hrAdmin.id,
+      email: hrAdmin.email,
+      role: hrAdmin.role,
+    });
+
+    // Log error and exit
+  } catch (error) {
+    console.error("Error seeding HR Admin:", error);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+// Batch seed employees (TO-DO)
+// ------------------------------------------------------
 /*
-set dedicated HRADMIN accn
-Admins are not employees
-*/
+ * not required for spec
+ * used for testing and performance
+ */
