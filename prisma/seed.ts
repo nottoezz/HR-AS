5a83dd7f4c78d301c6f8fec6e6d574a44ad4e250
nottoezz <liam@birch.co.za>
2026-01-25 12:50:52 +0200

    seeding: ensure .env gets process in npx

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,6 +9,7 @@
  */
 
 // Imports
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { hash } from "bcryptjs";
