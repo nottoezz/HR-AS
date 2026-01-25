5a48b02517115726ea8be238e95320bde01e1c6a
nottoezz <liam@birch.co.za>
2026-01-25 12:10:23 +0200

    seeding: setup seeding base to be built apon

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+/*
+* Seed script for the HR Admin System
+* ---------------------------------------------------------
+* - Create an initial HR Administrator account
+* -> username: hradming@test.com
+* -> password: TestPass1234
+* 
+* - Batch create employees (not apart of spec)
+*/
+
+// Imports
+
+// Prisma Client
+// Setup using BetterSqLite 3 adapter
+
+/*
+Main seed function
+Create prisma client
+Seed user
+Ensure relationships are correct
+Disconnect
+*/
+
+/*
+set dedicated HRADMIN accn
+Admins are not employees
+*/
