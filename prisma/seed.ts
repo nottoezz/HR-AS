e2541540aef6b460bd158752804ac98417efe8df
nottoezz <liam@birch.co.za>
2026-01-25 12:16:56 +0200

    seeding:set imports and create prisma client func

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,34 @@
 /*
-* Seed script for the HR Admin System
-* ---------------------------------------------------------
-* - Create an initial HR Administrator account
-* -> username: hradming@test.com
-* -> password: TestPass1234
-* 
-* - Batch create employees (not apart of spec)
-*/
+ * Seed script for the HR Admin System
+ * ---------------------------------------------------------
+ * - Create an initial HR Administrator account
+ * -> username: hradming@test.com
+ * -> password: TestPass1234
+ *
+ * - Batch create employees (not apart of spec)
+ */
 
 // Imports
+import { PrismaClient } from "@prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { hash } from "bcryptjs";
 
 // Prisma Client
-// Setup using BetterSqLite 3 adapter
+const createPrismaClient = () => {
+  const databaseURL = process.env.DATABASE_URL;
+
+  if (!databaseURL) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
+  const adapter = new PrismaBetterSqlite3({
+    url: databaseURL.replace("file:", ""),
+  });
+
+  return new PrismaClient({
+    adapter,
+  });
+};
 
 /*
 Main seed function
